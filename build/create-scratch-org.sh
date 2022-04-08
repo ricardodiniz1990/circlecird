bf5f5a867633f6d863262d76572af943cfd2787f
ricardodiniz1990 <56138076+ricardodiniz1990@users.noreply.github.com>
2022-04-08 17:28:01 +0100

    Update create-scratch-org.sh

--- build/create-scratch-org.sh
+++ build/create-scratch-org.sh
@@ -5,7 +5,8 @@ echo $SFDC_SERVER_KEY | base64 -di > keys/server.key
 
 # Authenticate to salesforce
 echo "Authenticating..."
-sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile keys/server.key --username $SFDC_PROD_USER --setdefaultdevhubusername -a DevHub
+#sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile keys/server.key --username $SFDC_PROD_USER --setdefaultdevhubusername -a DevHub
+sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile $SFDC_SERVER_KEY --username $SFDC_PROD_USER --setdefaultdevhubusername -a DevHub
 
 #Create a scratch org
 echo "Creating the Scratch Org..."
